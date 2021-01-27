bd7045b25f8c9700fb3e38c90437c4bc171642bd
Monika Yadav <40560838+patelashik@users.noreply.github.com>
2021-01-27 18:57:39 +0530

    Upload init

---
+++ precache-manifest.bb416818e19d468ed3d09cd879eec241.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b25ab486e70970eac652e4700f3b72",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "ec2c61b561ef05f40d64",
+    "url": "/ReactDemo/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "056b447d5a7159168408",
+    "url": "/ReactDemo/static/css/main.c0e3c836.chunk.css"
+  },
+  {
+    "revision": "ec2c61b561ef05f40d64",
+    "url": "/ReactDemo/static/js/2.4227d846.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactDemo/static/js/2.4227d846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056b447d5a7159168408",
+    "url": "/ReactDemo/static/js/main.3ecbaf32.chunk.js"
+  },
+  {
+    "revision": "87def2a8649f923aa3f9",
+    "url": "/ReactDemo/static/js/runtime-main.27714a8b.js"
+  },
+  {
+    "revision": "7ee134c2ac2f571565d40af9933cb25a",
+    "url": "/ReactDemo/static/media/navlogo.7ee134c2.png"
+  }
+]);
